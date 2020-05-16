0725f175422c1a48454f3298d4db6790587b2a99
Oliver Schmid <oli@breakthebox.ch>
2020-05-16 14:04:48 +0200

    update copyright

--- zskarte2/src/app/layers/layers.component.css
+++ zskarte2/src/app/layers/layers.component.css
@@ -1,18 +1,18 @@
 /*
- * Copyright © 2018 ZSO Bern Plus
+ * Copyright © 2018-2020 ZSO Bern Plus / PCi Fribourg
  *
  * This file is part of Zivilschutzkarte 2.
  *
  * Zivilschutzkarte 2 is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
+ * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
  * Zivilschutzkarte 2 is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
+ * You should have received a copy of the GNU Lesser General Public License along with
  * Zivilschutzkarte 2.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
